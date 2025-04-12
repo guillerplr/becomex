1e40ed544d18ac741581e0a0947ea84578437f0b
Guilherme Rezende <guillerplr@gmail.com>
2025-04-12 13:56:10 -0300

    download direcionado a pasta `/downloads` e pasta ignorada no `.gitignore`.

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import re
 from bs4 import BeautifulSoup
 
 url_base = 'https://arquivos.receitafederal.gov.br/cnpj/dados_abertos_cnpj/2025-03/'
+os.makedirs("downloads", exist_ok=True)
+
 
 def download_zip_files(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+        response.raise_for_status()
 
         soup = BeautifulSoup(response.text, 'html.parser')
         zip_links = []
@@ -20,20 +22,20 @@ def download_zip_files(url):
         if not zip_links:
             print('Nenhum arquivo .zip encontrado na página.')
             return
-
         for zip_url in zip_links:
             filename = os.path.basename(zip_url)
+            caminho = os.path.join("downloads",filename)
             try:
                 zip_response = requests.get(zip_url, stream=True)
                 zip_response.raise_for_status()
-                with open(filename, 'wb') as file:
+                with open(caminho, 'wb') as file:
                     for chunk in zip_response.iter_content(chunk_size=8192):
                         file.write(chunk)
                 print(f'Arquivo {filename} baixado com sucesso.')
             except requests.exceptions.RequestException as e:
                 print(f'Erro ao baixar {zip_url}: {e}')
             except IOError as e:
-                print(f'Erro ao salvar {filename}: {e}')
+                print(f'Erro ao salvar {caminho}: {e}')
 
     except requests.exceptions.RequestException as e:
         print(f'Erro ao acessar a página {url}: {e}')
